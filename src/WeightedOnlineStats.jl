ff8c81e143970bff92606ff4c841a586c236fae5
gdkrmr <gdkrmr@users.noreply.github.com>
2018-11-09 09:44:47 +0100

    fix type conversions

--- src/WeightedOnlineStats.jl
+++ src/WeightedOnlineStats.jl
@@ -121,45 +121,42 @@ end
 # end
 function _merge!(o::WeightedVariance{T}, o2::WeightedVariance) where T
 
-    convert(T, o2.W)
-    convert(T, o2.μ)
-    convert(T, o2.σ2)
-    convert(T, o2.W2)
+    o2_μ = convert(T, o2.μ)
+    o2_σ2 = convert(T, o2.σ2)
+    o2_W = convert(T, o2.W)
+    o2_W2 = convert(T, o2.W2)
 
-    W = o.W + o2.W
+    W = o.W + o2_W
     γ1 = o.W / W
-    γ2 = o2.W / W
+    γ2 = o2_W / W
 
-    μ = smooth(o.μ, o2.μ, γ2)
+    μ = smooth(o.μ, o2_μ, γ2)
 
     # o.σ2 =
     #     γ1 * ( o.σ2 +  o.μ ^ 2) +
-    #     γ2 * (o2.σ2 + o2.μ ^ 2) -
+    #     γ2 * (o2_σ2 + o2_μ ^ 2) -
     #     μ ^ 2
     o.σ2 =
         γ1 * ( o.σ2  + (o.μ - μ) ^ 2) +
-        γ2 * (o2.σ2 + (o2.μ - μ) ^ 2)
+        γ2 * (o2_σ2 + (o2_μ - μ) ^ 2)
 
     o.μ = μ
     o.W = W
-    o.W2 += o2.W2
-
-    ###########################################
+    o.W2 += o2_W2
 
     ###########################################
 
     # μ = o.μ
     #
-    # γ = o2.W / (o2.W + o.W)
-    # δ = o2.μ - o.μ
+    # γ = o2_W / (o2_W + o.W)
+    # δ = o2_μ - o.μ
     #
-    # o.σ2 = smooth(o.σ2, o2.σ2, γ) + (δ ^ 2) * γ * (1.0 - γ)
-    # o.μ = smooth(o.μ, o2.μ, γ)
-    # # o.σ2 = o.σ2 + (o.W + o2.W) * (μ)
+    # o.σ2 = smooth(o.σ2, o2_σ2, γ) + (δ ^ 2) * γ * (1.0 - γ)
+    # o.μ = smooth(o.μ, o2_μ, γ)
+    # # o.σ2 = o.σ2 + (o.W + o2_W) * (μ)
     #
-    # o.W += o2.W
-    # o.W2 += o2.W2
-    ########
+    # o.W += o2_W
+    # o.W2 += o2_W2
 
     return o
 end
