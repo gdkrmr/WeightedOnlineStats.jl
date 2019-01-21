cb4365682a6fade7ed041ab7a7fac7275ef10134
gdkrmr <gdkrmr@users.noreply.github.com>
2019-01-21 17:48:42 +0100

    update package to the new AbstractDataTransform in StatsBase

--- src/pca.jl
+++ src/pca.jl
@@ -1,18 +1,20 @@
+import StatsBase: ZScoreTransform
+export ZScoreTransform
+
 import MultivariateStats
 import MultivariateStats:
     PCA, pcacov, indim, outdim, projection, principalvar, principalvars,
     tprincipalvar, tresidualvar, tvar, principalratio, transform, reconstruct
 
 export
-    PCA, pcacov, indim, outdim, projection, principalvar, principalvars,
+    pca, PCA, pcacov, indim, outdim, projection, principalvar, principalvars,
     tprincipalvar, tresidualvar, tvar, principalratio, transform, reconstruct
 
-
 """
-    PCA(x::WeightedCovMatrix)
+    pca(x::WeightedCovMatrix)::Tuple{StatsBase.ZScoreTransform, MultivariateStats.PCA}
 
-Constructs a `MultivariateStats.PCA` object from a `WeightedCovMatrix`. For
-usage of the result, see the documentation of `MultivariateStats.jl`
+Constructs a `StatsBase.ZScoreTransform`, and a `MultivariateStats.PCA` object
+from a `WeightedCovMatrix`.
 
 # Parameters
 - `cov_pca::Bool = false`: Do a covariance or correlation pca.
@@ -23,25 +25,61 @@ usage of the result, see the documentation of `MultivariateStats.jl`
 
 # Example
     c = fit!(WeightedCovMatrix(), rand(100, 4), rand(100))
-    p = PCA(c)
+    t, p = pca(c)
     # note that for prediction/reconstruction with the `PCA` object, the observations are in columns!
-    transform(p, rand(4, 100))
-    reconstruct(p, rand(4, 100))
+    transform((t, p), rand(4, 100))
+    reconstruct(p, t), rand(4, 100))
 """
-function MultivariateStats.PCA(
-        x           :: WeightedCovMatrix;
+function pca(
+        x           :: WeightedCovMatrix{T};
         cov_pca     :: Bool          = false,
         maxoutdim   :: Int           = size(x, 1),
         pratio      :: AbstractFloat = 1.0,
         correct     :: Bool          = false,
         weight_type :: Symbol        = :analytic
-    )
+    ) where T
+
+    d = size(x, 1)
     c = cov_pca ?
         cov(x, corrected = correct, weight_type = weight_type) :
         cor(x, corrected = correct, weight_type = weight_type)
     m = mean(x)
     s = cov_pca ?
-        eltype(m)[] :
+        T[] :
         std(x, corrected = correct, weight_type = weight_type)
-    return MultivariateStats.pcacov(c, m, s, maxoutdim = maxoutdim, pratio = pratio)
+
+    t = ZScoreTransform(d, m, s)
+    p = MultivariateStats.pcacov(c, T[], maxoutdim = maxoutdim, pratio = pratio)
+
+    return t, p
+end
+
+function transform(t::Tuple{ZScoreTransform, PCA}, x)
+    xz = StatsBase.transform(t[1], x)
+    transform(t[2], xz)
+end
+
+function reconstruct(t::Tuple{PCA, ZScoreTransform}, x)
+    xz = reconstruct(t[1], x)
+    StatsBase.reconstruct(t[2], xz)
+end
+
+transform(t::Tuple{}, x) = x
+transform(t::Tuple{T}, x) where T = transform(t, x)
+function transform(t::Tuple, x)
+    xt = transform(x[1])
+    for i in 2:length(t)
+        xt = transform(t[i], xt)
+    end
+    xt
+end
+
+reconstruct(t::Tuple{}, x) = x
+reconstruct(t::Tuple{T}, x) where T = reconstruct(t, x)
+function reconstruct(t::Tuple, x)
+    xt = reconstruct(x[1])
+    for i in 2:length(t)
+        xt = reconstruct(t[i], xt)
+    end
+    xt
 end
--- test/test_pca.jl
+++ test/test_pca.jl
@@ -1,3 +1,5 @@
+using StatsBase
+
 """
 the eigenvectors have arbitrary directions.
 """
@@ -10,26 +12,43 @@ end
 
     x = rand(100, 4)
     w = ones(100)
+    zsm = fit(ZScoreTransform, x')
+    zm = fit(ZScoreTransform, x', scale = false)
+
     c = fit!(WeightedCovMatrix(), x, w)
 
-    p1 = PCA(c, cov_pca = true)
-    p6 = fit(PCA, x', method = :cov, maxoutdim = 4, pratio = 1.0, mean = nothing, std = 1.0)
+    t, p = pca(c, cov_pca = true)
+    p6 = fit(
+        PCA,
+        StatsBase.transform(zm, permutedims(x)),
+        method = :cov,
+        maxoutdim = 4,
+        pratio = 1.0,
+        mean = nothing
+    )
 
-    @test p1.mean ≈ p6.mean
-    @test p1.std ≈ p6.std
-    @test isapprox(p1.prinvars, p6.prinvars, atol = 1.0e-2)
-    @test isapprox(p1.tprinvar, p6.tprinvar, atol = 1.0e-2)
-    @test isapprox(p1.tvar, p6.tvar, atol = 1.0e-2)
-    @test test_proj(p1.proj, p6.proj)
+    @test t.mean ≈ zm.mean
+    @test t.scale ≈ zm.scale
+    @test isapprox(p.prinvars, p6.prinvars, atol = 1.0e-2)
+    @test isapprox(p.tprinvar, p6.tprinvar, atol = 1.0e-2)
+    @test isapprox(p.tvar, p6.tvar, atol = 1.0e-2)
+    @test test_proj(p.proj, p6.proj)
 
-    p2 = PCA(c, cov_pca = false)
-    p4 = fit(PCA, x', method = :cov, maxoutdim = 4, pratio = 1.0, mean = nothing, std = nothing)
+    t, p = pca(c, cov_pca = false)
+    p4 = fit(
+        PCA,
+        StatsBase.transform(zsm, x'),
+        method = :cov,
+        maxoutdim = 4,
+        pratio = 1.0,
+        mean = 0
+    )
 
-    @test p2.mean ≈ p4.mean
-    @test isapprox(p2.std, p4.std, atol = 1.0e-2)
-    @test isapprox(p2.prinvars, p4.prinvars, atol = 1.0e-2)
-    @test isapprox(p2.tprinvar, p4.tprinvar, atol = 1.0e-2)
-    @test isapprox(p2.tvar, p4.tvar, atol = 1.0e-2)
-    @test test_proj(p2.proj, p4.proj)
+    @test t.mean ≈ zsm.mean
+    @test isapprox(t.scale, zsm.scale, atol = 1.0e-2)
+    @test isapprox(p.prinvars, p4.prinvars, atol = 1.0e-2)
+    @test isapprox(p.tprinvar, p4.tprinvar, atol = 1.0e-2)
+    @test isapprox(p.tvar, p4.tvar, atol = 1.0e-2)
+    @test test_proj(p.proj, p4.proj)
 
 end
