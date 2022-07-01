2c6fd24ef16876c915d2bef140821f50fa759486
Guido Kraemer <guido.kraemer@uni-leipzig.de>
2022-07-01 11:36:51 +0200

    set CNAME in documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,10 @@
 using Documenter, WeightedOnlineStats
 
 makedocs(sitename = "WeightedOnlineStats.jl")
+
+cname = open("docs/build/CNAME")
+write(cname, "www.guido-kraemer.com")
+close(cname)
+
 deploydocs(repo = "github.com/gdkrmr/WeightedOnlineStats.jl.git",
            push_preview = true)
