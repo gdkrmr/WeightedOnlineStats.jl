7dd5664ac7ef9b7f0855c697598b4f6a5e26ecf1
Guido Kraemer <guido.kraemer@uni-leipzig.de>
2022-07-01 11:48:52 +0200

    don't add CNAME to every build

--- docs/make.jl
+++ docs/make.jl
@@ -2,9 +2,5 @@ using Documenter, WeightedOnlineStats
 
 makedocs(sitename = "WeightedOnlineStats.jl")
 
-cname = open("docs/build/CNAME", "w")
-write(cname, "www.guido-kraemer.com")
-close(cname)
-
 deploydocs(repo = "github.com/gdkrmr/WeightedOnlineStats.jl.git",
            push_preview = true)
