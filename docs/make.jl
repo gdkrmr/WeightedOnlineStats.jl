24a7e6b41defda09d838464b6239a7335aa261a5
Guido Kraemer <gdkrmr@users.noreply.github.com>
2022-07-01 11:12:45 +0200

    start documentation (#47)
    * start documentation
    
    * fix ci
    
    * fix ci
    
    * ci didn't run before :shrug:
    
    * fix ci
    
    * fix ci
    
    * allow preview of documentation

---
+++ docs/make.jl
@@ -0,0 +1,5 @@
+using Documenter, WeightedOnlineStats
+
+makedocs(sitename = "WeightedOnlineStats.jl")
+deploydocs(repo = "github.com/gdkrmr/WeightedOnlineStats.jl.git",
+           push_preview = true)
