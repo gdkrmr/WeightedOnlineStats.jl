05354fffca5f69e406d20cb59d017d0cced90381
Guido Kraemer <guido.kraemer@uni-leipzig.de>
2022-07-01 11:05:21 +0200

    allow preview of documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,5 @@
 using Documenter, WeightedOnlineStats
 
 makedocs(sitename = "WeightedOnlineStats.jl")
-deploydocs(repo = "github.com/gdkrmr/WeightedOnlineStats.jl.git")
+deploydocs(repo = "github.com/gdkrmr/WeightedOnlineStats.jl.git",
+           push_preview = true)
